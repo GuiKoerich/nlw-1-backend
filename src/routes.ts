d4281c274c9c255e36fac325ba76635a4e18ec59
Guilherme <guilhermekoerich@gmail.com>
2020-06-06 16:22:45 -0300

    Validação de campos

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 
 import ItemsController from './controllers/itemsController';
 import PointsController from './controllers/pointsController';
@@ -15,7 +16,27 @@ const pointsController = new PointsController();
 routes.get('/items', itemsController.index);
 
 routes.get('/points', pointsController.index);
-routes.get('/points/:id', pointsController.show);
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.get('/points/:id', 
+  celebrate({
+    params: Joi.number().required(),
+  }), 
+  pointsController.show);
+routes.post('/points', 
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      phone: Joi.string().required(),
+      lat: Joi.number().required(),
+      lon: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    }), 
+  }, {
+    abortEarly: false,
+  }),
+  pointsController.create);
 
 export default routes;
